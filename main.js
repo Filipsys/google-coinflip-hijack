92a736666832dda87ebb46e774e94b9182aeec67
filyys <93247526+Filipsys@users.noreply.github.com>
2025-01-27 15:04:13 +0100

    Updated logic

--- main.js
+++ main.js
@@ -4,58 +4,130 @@
 // @version      2025-01-26
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.google.com/search?hl=en&q=coin%20flip
+// @match        https://www.google.com/search?q=coin+flip
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // @run-at document-end
 // ==/UserScript==
 
-(async function () {
-  "use strict";
-  const imagesLink = "https://www.google.com/logos/fnbx/coin_flip/coin_sheet.png";
-  const imageYLocations = [
-    // May or may not be off by one
-    0, 150, 300, 450, 600, 750, 900, 1050, 1200, 1350, 1500, 1650, 1800, 1950, 2100, 2250, 2400,
-  ];
-  let canvasFound = false;
-  let canvas;
-  let currentImageIndex = 0;
-  let rotation = 0;
+(async function() {
+    'use strict';
 
-  while (!canvasFound) {
-    canvas = document.querySelector("canvas");
-    if (canvas) canvasFound = true;
+    const imagesLink = "https://www.google.com/logos/fnbx/coin_flip/coin_sheet.png"
+    const imageYLocations = [
+        0, 150, 300, 450, 600,
+        750, 900, 1050, 1200,
+        1350, 1500, 1650, 1800,
+        1950, 2100, 2250, 2400
+    ];
+    const resultIndicies = {
+        heads: 0, tails: 8
+    };
 
-    await new Promise((resolve) => setTimeout(resolve, 100));
-  }
+    let rotationDirection = "clockwise"; // counter-clockwise or clockwise
+    let flipDirection = "clockwise";
+    let desiredResult = "tails";
+    let spinningTimeInMs = 3000;
+    let maxFullRotationCount = 5;
+    let maxFullSpinCount = 5;
+    let rotationSpeed = 4;
+    let flipSpeed = 1;
+    let canvasFound = false;
+    let currentImageIndex = 0;
+    let currentFullRotationCount = 0;
+    let currentFullSpinCount = 0;
+    let rotation = 0;
+    let timeStart = 0;
+    let canvas;
 
-  const newImages = document.querySelector("img[src='/logos/fnbx/coin_flip/coin_sheet.png']");
-  newImages.style.display = "none";
+    let timePerSpin = spinningTimeInMs / maxFullSpinCount;
+    let timePerFrame = timePerSpin / 18 // There are 18 frames in the image
 
-  const newCanvas = document.createElement("canvas");
-  const ctx = newCanvas.getContext("2d");
-  newCanvas.width = 150;
-  newCanvas.height = 150;
-  newCanvas.zIndex = "1000";
-  canvas.replaceWith(newCanvas);
+    /* const randomPresets = [
+        {
+            spinningTimeInMs: 1000,
+            maxFullRotationCount: 2
+        }, {
+            spinningTimeInMs: 3000,
+            maxFullRotationCount: 4,
+            maxFullSpinCount: 6
+        }, {
+            spinningTimeInMs: 3000,
+            maxFullRotationCount: 3
+        }, {
+            spinningTimeInMs: 3000,
+            maxFullRotationCount: 1
+        }
+    ] */
 
-  const playLoop = () => {
-    ctx.clearRect(0, 0, 150, 150);
+    while (!canvasFound) {
+        canvas = document.querySelector("canvas");
+        if (canvas) canvasFound = true;
 
-    ctx.save();
-    ctx.translate(75, 75);
-    ctx.rotate((rotation * Math.PI) / 180);
-    ctx.translate(-75, -75);
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
 
-    ctx.drawImage(newImages, 0, imageYLocations[currentImageIndex], 150, 150, 0, 0, 150, 150);
+    const newImages = document.querySelector("img[src='/logos/fnbx/coin_flip/coin_sheet.png']");
+    newImages.style.display = "none";
 
-    ctx.restore();
+    const newCanvas = document.createElement("canvas");
+    const ctx = newCanvas.getContext('2d');
+    newCanvas.width = 150
+    newCanvas.height = 150
+    newCanvas.zIndex = "1000"
+    canvas.replaceWith(newCanvas);
 
-    currentImageIndex = currentImageIndex < imageYLocations.length - 1 ? currentImageIndex + 1 : 0;
-    rotation = rotation < 360 ? rotation + 2 : (rotation = 0);
+    const playLoop = () => {
+//        const timeNow = Date.now();
+//        if (timeNow > timeStart + spinningTimeInMs) {
+//            if (currentImageIndex == resultIndicies[desiredResult]) return cancelAnimationFrame(playLoop)
+//        }
+        if (currentFullRotationCount >= maxFullRotationCount) return cancelAnimationFrame(playLoop)
 
-    requestAnimationFrame(playLoop);
-  };
+        ctx.clearRect(0, 0, 150, 150);
 
-  window.requestAnimationFrame(playLoop);
+        ctx.save();
+        ctx.translate(75, 75);
+        ctx.rotate(rotation * Math.PI / 180);
+        ctx.translate(-75, -75);
+
+        ctx.restore();
+
+        if (flipDirection == "clockwise") {
+            if (currentImageIndex < imageYLocations.length - 1) {
+                currentImageIndex = currentImageIndex + flipSpeed;
+            } else {
+                currentImageIndex = 0;
+                currentFullSpinCount++;
+            }
+        } else {
+            if (currentImageIndex <= 0) {
+                currentImageIndex = imageYLocations.length - 1;
+                currentFullSpinCount++;
+            } else {
+                currentImageIndex = currentImageIndex - flipSpeed;
+            }
+        }
+
+        if (rotationDirection == "clockwise") {
+             if (rotation + rotationSpeed < 360) {
+                 rotation = rotation + rotationSpeed;
+             } else {
+                 rotation = 0;
+                 currentFullRotationCount++;
+             }
+        } else {
+            if (rotation - rotationSpeed <= 0) {
+                rotation = rotation - rotationSpeed;
+            } else {
+                rotation = 360;
+                 currentFullRotationCount++;
+            }
+        }
+
+        requestAnimationFrame(playLoop);
+    };
+
+    timeStart = Date.now();
+    window.requestAnimationFrame(playLoop);
 })();
